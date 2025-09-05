e880ae504d306da0467521cd7e2bd7eaefe7c2c4
Mounika-Orotron <mounika.s@orotron.com>
2025-09-05 12:44:55 +0530

    Update swiper OurProcess Section

--- src/routes/crazyorbit.tsx
+++ src/routes/crazyorbit.tsx
@@ -1,7 +1,7 @@
-
 import { AboutPage } from "@/components/CrazyOrbit";
+import { CrazyOrbitIndex } from "@/components/CrazyOrbit/htmlFile";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/crazyorbit")({
-  component: AboutPage,
+  component: CrazyOrbitIndex,
 });
